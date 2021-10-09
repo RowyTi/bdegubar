f114bdc7745d2177972dedbccb17aadba9323999
Rodrigo <rodrigotilli@gmail.com>
2021-10-09 16:52:20 -0300

    instalacion paquete laravel/sanctum autenticacion api

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that aren't mass assignable.
