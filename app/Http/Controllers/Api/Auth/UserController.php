f92ff5f9e0ade8e2bd9bcb2e2920d9978b09c966
Rodrigo <rodrigotilli@gmail.com>
2021-10-20 11:28:20 -0300

    Update UserController.php
    Se creo respuesta de usuario autenticado con la busqueda de permisos dinamico

--- app/Http/Controllers/Api/Auth/UserController.php
+++ app/Http/Controllers/Api/Auth/UserController.php
@@ -18,8 +18,27 @@ class UserController extends Controller
     public function __invoke(Request $request): JsonResponse
     {
         $user = $request->user();
-        $user->scope = ['super-admin', 'view:dashboard', '*'];
+        
+        // $user->scope = ['view:dashboard', 'view:user', 'administracion'];
+        // $user->scope = $user->getPermissionNames();
 
-        return response()->json($user);
+        // branch_id: 12
+        // created_at: "2021-10-18T05:04:38.000000Z"
+        // id: 3
+        // permissions: [{id: 53, name: "view:user", guard_name: "sanctum", created_at: "2021-10-18T05:04:37.000000Z",…},…]
+        // profile_id: 38
+        // scope: ["view:user", "show:user"]
+        // state: "activo"
+        // updated_at: "2021-10-18T05:04:38.000000Z"
+        // username: "htoy"
+
+        return response()->json([
+            'id'        => $user->id,
+            'username'  => $user->username,
+            'scope'     => $user->getPermissionNames(),
+            'state'     => $user->state,
+            'branch_id' => $user->branch_id,
+            'profile_id'=> $user->profile_id
+        ]);
     }
 }
