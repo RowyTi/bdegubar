bad8b522420c3915066ffced744f19369fc9c482
Rodrigo <rodrigotilli@gmail.com>
2021-10-25 23:28:02 -0300

    staff Test
    Se realizaron test de admin:staff y usuario comun, para las rutas index y read.

--- tests/Feature/Staff/ListStaffTest.php
+++ tests/Feature/Staff/ListStaffTest.php
@@ -12,14 +12,74 @@ class ListStaffTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function solo_pueden_listar_staff_pertenecientes_a_un_branch()
+    // se verifica que un usuario del mismo staff puede ver a otros miembros del staff
+    public function solo_pueden_listar_staff_pertenecientes_a_un_mismo_branch()
     {
         $staff = Staff::factory()->create();
 
         Sanctum::actingAs($staff, ['index:staff']);
 
         $this->jsonApi()
-            ->get(route('v1.staff.index'))
+            ->get(route('v1.staff.index', ['filter[branch_id]=1']))
             ->assertStatus(200);
+    }
+
+    /** @test */
+    // se verifica que un usuario del de otro branch no pueda ver los empleados de otra sucursal
+    public function un_usuario_no_puede_listar_staff_de_otro_branch()
+    {
+        $staff = Staff::factory()->create();
+
+        Sanctum::actingAs($staff, ['index:staff']);
+
+        $this->jsonApi()
+            ->get(route('v1.staff.index', ['filter[branch_id]=4']))
+            ->assertStatus(403);
+    }
+    /** @test */
+    public function un_usuario_puede_ver_sus_datos(){
+        $staff = Staff::factory()->create();
+
+        Sanctum::actingAs($staff, ['read:staff']);
+
+        $this->jsonApi()
+            ->get(route('v1.staff.read', $staff))
+            ->assertStatus(200);
+    }
+
+    /** @test */
+    public function un_administrador_de_puede_ver_los_datos_de_sus_empleados(){
+        $staff = Staff::factory()->create(); // me logueo como adm
+        $staff2 = Staff::factory()->create(['branch_id'=> $staff->branch_id]); // creo otro usuario dentro del branch
+
+        Sanctum::actingAs($staff, ['admin:staff']);
+
+        $this->jsonApi()
+            ->get(route('v1.staff.read', $staff2))
+            ->assertStatus(200);
+    }
+
+    /** @test */
+    public function un_administrador_no_puede_ver_los_datos_de_empleados_de_otro_branch(){
+        $staff = Staff::factory()->create(); // me logueo como adm
+        $staff2 = Staff::factory()->create(); // creo otro usuario dentro del branch
+
+        Sanctum::actingAs($staff, ['admin:staff']);
+
+        $this->jsonApi()
+            ->get(route('v1.staff.read', $staff2))
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    public function un_usuario_no_puede_ver_los_datos_de_otro_usuario(){
+        $staff = Staff::factory()->create();
+
+        $other_user = Staff::factory()->create(['id'=>'123']);
+        Sanctum::actingAs($staff, ['read:staff']);
+
+        $this->jsonApi()
+            ->get(route('v1.staff.read', $other_user))
+            ->assertStatus(403);
     }
 }
